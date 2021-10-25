d5b44f2a4896f2776d52eb66b2920c9bf892b885
Or Posener <30498431+poseneror@users.noreply.github.com>
2021-10-25 18:20:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+
+function getParameterByName(name, url) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 console.log('loaded script');
+Wix.Worker.getSiteInfo(siteInfo => {
+  const hostUrl = siteInfo.url;
+  const referrer = getParameterByName('referrer', hostUrl);
+  console.log('referred by', referrer);
+})
